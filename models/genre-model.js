c862c78456f5a6e928109dc8b7c543a2e22dfbb7
Daxe9 <davidexie694@gmail.com>
2022-02-10 22:02:12 +0100

    tested endpoint

--- models/genre-model.js
+++ models/genre-model.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const genreSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 100,
-    },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 50,
+  },
 });
 
 const Genre = mongoose.model("Genre", genreSchema);
 
 function validateRequest(req) {
-    const schema = Joi.object({
-        genre: Joi.string().required(),
-    });
-    return schema.validate(req.body);
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+  });
+  return schema.validate(req.body);
 }
 
 module.exports.Genre = Genre;
--- startup/routes.js
+++ startup/routes.js
@@ -8,31 +8,31 @@ const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
-const loggerTime = require("../middleware/logger");
+// const loggerTime = require("../middleware/logger");
 const error = require("../middleware/error");
 let logStream = fs.createWriteStream(path.join("./log/", "access.log"), {
-    flags: "a",
+  flags: "a",
 });
 
 module.exports = function (app) {
-    // middleware use json file to request
-    app.use(express.json());
-    // use customized middleware ./middleware/logger.js
-    app.use(loggerTime);
-    // get information from user and then write it into access.log
-    app.use(morgan("combined", { stream: logStream }));
-    // router /api/genres
-    app.use("/api/genres", genres);
-    // router /api/customers
-    app.use("/api/customers", customers);
-    // router /api/movies
-    app.use("/api/movies", movies);
-    // router /api/rentals
-    app.use("/api/rentals", rentals);
-    // router /api/users
-    app.use("/api/users", users);
-    // router /aopi/auth
-    app.use("/api/auth", auth);
+  // middleware use json file to request
+  app.use(express.json());
+  // use customized middleware ./middleware/logger.js
+  // app.use(loggerTime);
+  // get information from user and then write it into access.log
+  app.use(morgan("combined", { stream: logStream }));
+  // router /api/genres
+  app.use("/api/genres", genres);
+  // router /api/customers
+  app.use("/api/customers", customers);
+  // router /api/movies
+  app.use("/api/movies", movies);
+  // router /api/rentals
+  app.use("/api/rentals", rentals);
+  // router /api/users
+  app.use("/api/users", users);
+  // router /aopi/auth
+  app.use("/api/auth", auth);
 
-    app.use(error);
+  app.use(error);
 };
